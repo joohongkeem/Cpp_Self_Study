06ca6d040357db75d6412257f4f7143a3d882d07
joohongkeem <38645351+joohongkeem@users.noreply.github.com>
2018-08-01 14:54:28 +0900

    0801

---
+++ _037_StringClass.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARINGS
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+	* 문자열을 다루는 두 가지 방법
+		1) C-string 
+
+		2) string클래스 
+
+		- <string> 라이브러리에 정의되어 있으며, 정의문들은 std 네임스페이스에 명시되어 있다.
+		> 따라서 프로그램에서 string 클래스를 사용하기 위해서는 
+			#include <string>
+			using namespace std; 
+		  의 과정이 필요하다
+
+		- string 클래스는 string 값과 string 변수들을 간단하게 조작할 수 있게 해준다.
+		> '= 연산자'를 이용하여 string 변수에 값을 할당할 수 있으며,
+		> '+ 연산자'를 이용하여 2개의 string을 서로 연결할 수 있다.
+			ex) s3 = s1 + s2; 를 하면, s3의 저장공간보다 s1과 s2를 합친 스트링의 길이가 큰 경우
+				s3의 저장공간이 자동적으로 증가하게 된다.
+
+		- s3 = "Hello World\n"; 라는 코드를 작성하면,
+		  실제 "Hello World\n"은 C-String에 속하지만 string형의 데이터는 아니지만, 자동으로 string형으로 형 변환을 해준다.
+
+		- string 클래스에는 3개의 생성자가 있다.
+			1) string str;
+				- 디폴트 생성자, str 객체 이름을 갖는 string 생성
+
+			2) string str("string");
+				- "string" 값을 가지는 string 객체 생성
+
+			3) string str(aString);
+				- string 클래스의 객체 aString의 값을 복사한 str 객체 이름을 갖는 string 생성
+
+*/
+
+
+
---
+++ _039_StringClass(string,C-string).cpp
@@ -0,0 +1,26 @@
+﻿
+/* 
+	* string 객체와 C-String의 상호 변환
+
+	- C++에서는 string형 변수에 C-String 값을 저장하는 경우 자동적으로 형 변환이 일어난다.
+		char aCString[] = "This is my C-String.";
+		string stringVariable;
+		stringVariable = aCString;
+	> 즉, 위의 코드는 아무런 문제가 없다.
+	
+	  But, 다음과 같은 코드들은 허용되지 않는다.
+		aCString = stringVariable;			(XXXXX)
+		strcpy(aCString, stringVariable);	(XXXXX)
+
+	  Why? strpcy함수의 두 번째 인자는 string 객체를 취할 수 없으며
+		   string객체는 C-String으로 자동적으로 변환되지 않는다.
+
+	★★★★★★★★★★★
+    - C-String을 string 객체로 변환하기 위해서는 반드시 명시적인 형 변환을 수행하여야 한다.
+	  이를 위해서, string 멤버 함수인 c_str() 함수를 사용할 수 있다.
+		strcpy(aCString, stringVariable.c_str());	(OOOOO)
+
+	  But, 할당 연산자를 사용하여 C-String을 처리할 수 없기 때문에 다음과 같은 문장을 허용되지 않는다.
+		aCString = stringVariable.c_str();			(XXXXX)
+
+*/
